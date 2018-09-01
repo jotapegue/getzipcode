a5b3a3a975e567a843900cd097be289648a26b65
João Pedro Garcia <pedro.garcia09@hotmail.com>
2018-09-01 15:51:38 -0300

    adicionando log.txt e gitignore

--- index.php
+++ index.php
@@ -18,7 +18,10 @@ class GetZipCode
     {
 
         $hourInital = date('d/m/Y H:i:s');
+        $file = "log.txt";
+        $log = fopen($file, "a+");
         print $hourInital . PHP_EOL;
+        fwrite($log, $hourInital."\n");
         $countSave = 0;
         foreach (range($this->initialZipCode, $this->finalZipCode) as $number) {
             $url = "https://viacep.com.br/ws/{$number}/json/";
@@ -28,17 +31,21 @@ class GetZipCode
                 print "cep não encontrado". PHP_EOL;
             }else{
                 if($getJson->localidade == "Santarém")
+                fwrite($log, "CEP: {$number} encontrado \n");
                 print "cadastrando cep" . PHP_EOL;
                 $countSave++;
             }
         }
         $hourFinal = date('d/m/Y H:i:s');
         print $hourFinal . PHP_EOL;
+        fwrite($log, $hourFinal."\n");
         print "Total de {$countSave} ceps cadastrados" . PHP_EOL;
+        fwrite($log, "Total de {$countSave} ceps encontrados \n");
+        fclose($log);
     }
 
 }
 
-// $get = new GetZipCode(68020050, 68020055);
-$get = new GetZipCode;
+$get = new GetZipCode(68020050, 68020055);
+// $get = new GetZipCode;
 $get->get();
